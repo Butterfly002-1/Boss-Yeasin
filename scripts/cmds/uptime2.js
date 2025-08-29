0047d47a444501873fca3277c3a4c0aeaa8d01b0
Butterfly002-1 <yeasinbhai2010@gmail.com>
2025-08-29 22:01:34 +0600

    Update uptime2.js

--- scripts/cmds/uptime2.js
+++ scripts/cmds/uptime2.js
@@ -52,7 +52,7 @@ module.exports = {
       + `• 𝐒𝐞𝐫𝐯𝐞𝐫: ${systemInfo.systemUptime}\n`
       + `\n[ 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 ]\n`
       + `• 𝐌𝐞𝐦𝐨𝐫𝐲: ${systemInfo.processMemory}\n`
-      + `╚═━「𝐁𝐚𝐘𝐣𝐢𝐝 𝐁𝐎𝐓」━═╝`;
+      + `╚═━「𝒀𝒆𝒂𝒔𝒊𝒏 𝑩𝒐𝒕」━═╝`;
 
     message.reply(response);
   }
