28d396954e61d8da5c3f44d13cd25050b3175f26
Butterfly002-1 <yeasinbhai2010@gmail.com>
2025-08-31 02:27:44 +0600

    Update supportgc.js

--- scripts/cmds/supportgc.js
+++ scripts/cmds/supportgc.js
@@ -11,7 +11,7 @@ module.exports = {
     longDescription: {
       en: "Join the official support group chat"
     },
-    category: "BUTTERFLY SUPPORT BOX",
+    category: "admin",
     guide: {
       en: "{pn}"
     }
