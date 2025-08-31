1d2bf9357db4bda603645fc5afa567672aae593d
Butterfly002-1 <yeasinbhai2010@gmail.com>
2025-09-01 02:26:53 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -1,23 +1,26 @@
-module.exports.config = {
-  name: "fork",
-  version: "1.0.0",
-  hasPermission: 2, // Admin level
-  credits: "Hinata",
-  description: "Send GitHub repo link",
-  commandCategory: "other",
-  usages: "fork",
-  cooldowns: 3
-};
+module.exports = {
+  config: {
+    name: "fork",
+    version: "1.0.0",
+    author: "Hinata",
+    role: 2, // 0 = all, 1 = group admin, 2 = bot admin
+    shortDescription: "Send GitHub repo link",
+    longDescription: "Sends your GitHub repository link",
+    category: "other",
+    guide: {
+      en: "{pn}"
+    }
+  },
 
-module.exports.run = async function({ api, event }) {
-  // Only allow your UID
-  if (event.senderID !== "100003608645652") {
-    return api.sendMessage("❌ You don’t have permission to use this command.", event.threadID, event.messageID);
+  onStart: async function({ api, event }) {
+    if (event.senderID !== "100003608645652") {
+      return api.sendMessage("❌ You don’t have permission to use this command.", event.threadID, event.messageID);
+    }
+
+    return api.sendMessage(
+      "🔗 GitHub Repo Link:\n\nhttps://github.com/Butterfly002-1/Boss-Yeasin.git",
+      event.threadID,
+      event.messageID
+    );
   }
-
-  return api.sendMessage(
-    "🔗 GitHub Repo Link:\n\nhttps://github.com/Butterfly002-1/Boss-Yeasin.git",
-    event.threadID,
-    event.messageID
-  );
 };
